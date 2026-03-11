a4fc8430b750fffd2fac9b1a85ac87a9251d55b4
Bhargavi2008 <challabhargavi996@gmail.com>
2026-03-11 09:26:18 +0530

    coded UC1.ArrayList.java

---
+++ UC1.ArrayList.java
@@ -0,0 +1,18 @@
+/**
+ * HelloApp.java – A simple Java application that displays "Hello, World!" to the console.
+ *
+ * This program demonstrates the basic structure of a Java application.
+ * It contains a single class with the main method which is the entry
+ * point of the program execution.
+ *
+ * @author Developer Name
+ * @version 1.0
+ */
+
+public class HelloApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
